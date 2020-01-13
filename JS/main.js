295111c214347e12e21514d63f26ac87e65e53a6
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-13 20:10:08 +0600

    done up  to CODING CHALLENGE -3

--- JS/main.js
+++ JS/main.js
@@ -3,25 +3,27 @@
 
 //  *************************/
 
+console.log("CODING CHALLENGE -1");
+
  
-//  var markMass, markHeight, markBMI,johnMass, johnHeight,johnBMI;
-//  markHeight = prompt('what is Marks height in  meter?');
-//  console.log(markHeight*0.3048);
-//  markMass = prompt('what is Marks mass?');
-//  console.log(markMass);
-//  markBMI = markMass/(markHeight^2);
-//  console.log('BMI of Mark '+markBMI);
+ var markMass, markHeight, markBMI,johnMass, johnHeight,johnBMI;
+ markHeight = prompt('what is Marks height in  meter?');
+ console.log(markHeight*0.3048);
+ markMass = prompt('what is Marks mass?');
+ console.log(markMass);
+ markBMI = markMass/(markHeight^2);
+ console.log('BMI of Mark '+markBMI);
 
-//  johnHeight = prompt('what is Johns height in  meter?');
-//  console.log(johnHeight*0.3048);
-//  johnMass = prompt('what is Johns mass?');
-//  console.log(johnMass);
+ johnHeight = prompt('what is Johns height in  meter?');
+ console.log(johnHeight*0.3048);
+ johnMass = prompt('what is Johns mass?');
+ console.log(johnMass);
 
-//  johnBMI= johnMass/(johnMass^2);
-//  console.log('BMI of John '+johnBMI);
+ johnBMI= johnMass/(johnMass^2);
+ console.log('BMI of John '+johnBMI);
 
-//  var diffrence  =markBMI> johnBMI;
-//  console.log('Is Marks  BMI higher than Johns BMI? ' + diffrence);
+ var diffrence  =markBMI> johnBMI;
+ console.log('Is Marks  BMI higher than Johns BMI? ' + diffrence);
 
  
 
@@ -31,6 +33,7 @@
 
 //  *************************/
 
+console.log("\n\nCODING CHALLENGE -2\n");
 
 
 
@@ -59,3 +62,41 @@ else if (marysTeamScore>johnsTeamScore && marysTeamScore>mikesTeamScore)
 else{
     console.log("matches are draw");
 }
+
+
+// /************************
+// CODING CHALLENGE -3
+
+// 
+
+console.log("\n\nCODING CHALLENGE -3\n");
+
+
+var bills =[124,48,268];
+function tipCalculate(bill){
+    var tips;
+
+    if(bill<50){
+        tips = (20/100);
+
+    }
+    else if(bill>=50 && bill<200){
+        tips =(15/100);
+
+    }
+    else if(bill>=200){
+        tips = (10/100);
+
+    }
+
+    return tips*bill;
+}
+
+
+var totalTips =[tipCalculate(bills[0]),tipCalculate(bills[1]),tipCalculate(bills[2])];
+
+var finalBill = [
+    bills[0]+totalTips[0],bills[1]+totalTips[1],bills[2]+totalTips[2],]
+
+
+console.log(totalTips,finalBill);
