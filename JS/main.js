8869155954e6c17c41203a5992668db637f4df9b
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-13 17:16:34 +0600

    CODING CHALENGE-1

--- JS/main.js
+++ JS/main.js
@@ -1,64 +1,32 @@
-// var fristName = "john";
-// var lastName = "doe";
+/************************
+CODING CHALENGE
 
-// console.log('First Name'+fristName +' '+'Last Name'+lastName);
+ *************************/
 
-// var  age = 28;
 
-// var  fullAge = true;
-// console.log( 'john  is '+age+' years  old '+'and  it is '+fullAge);
+ var markMass, markHeight, markBMI,johnMass, johnHeight,johnBMI;
+ markHeight = prompt('what is Marks height in  meter?');
+ console.log(markHeight*0.3048);
+ markMass = prompt('what is Marks mass?');
+ console.log(markMass);
+ markBMI = markMass/(markHeight^2);
+ console.log('BMI of Mark '+markBMI);
+
+ johnHeight = prompt('what is Johns height in  meter?');
+ console.log(johnHeight*0.3048);
+ johnMass = prompt('what is Johns mass?');
+ console.log(johnMass);
+
+ johnBMI= johnMass/(johnMass^2);
+ console.log('BMI of John '+johnBMI);
+
+ var diffrence  =markBMI> johnBMI;
+ console.log('Is Marks  BMI higher than Johns BMI? ' + diffrence);
+
  
-// var job;
-// console.log(job);
-
-// job = 'teacher';
-
-// console.log('he  is a '+job);
-// //var naming  rules
-// /*fhff
-// hhhhh*/
-
-// var _3year = 3;
-// console.log(_3year);
-
-// var  job,  isMarried;
-
-// job = "teacher";
-// isMarried = false;
-
-// /************************** */
-// //typing coerection//
-
-// console.log(fristName+ ' is  a '+job+'. he is '+age+' years  old.'+ 'but is he married? '+isMarried);
-
-
-// /*******************************/
-
-// //variable  mutation== changing  value  of a  variable
-
-// age='tweenty eight';
-
-// console.log(age);
-
-
-// alert(fristName+ ' is  a '+job+'. he is '+age+' years  old.'+ 'but is he married? '+isMarried);
-
-
-// var lastName = prompt('what  is  his  lastname?');
-
-// console.log(lastName);
-
-
-// console.log(fristName+" "+lastName);
-
-
-// console.log("hello   World  of JAVASCRIPT!!!!! I  am  new Here")
+ 
 
 
 
-var yearJohn = 2018-28;
-
-console.log(yearJohn);
-
-
-console.log("hello   World  of JAVASCRIPT!!!!! I  am  new Here")
+ 
+ 
