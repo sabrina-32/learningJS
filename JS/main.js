351b7a1368dd765a1fb8e57482d05b005cc55f02
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-13 16:05:35 +0600

    learn about coerection & mutatuin

---
+++ JS/main.js
@@ -0,0 +1,56 @@
+var fristName = "john";
+var lastName = "doe";
+
+console.log('First Name'+fristName +' '+'Last Name'+lastName);
+
+var  age = 28;
+
+var  fullAge = true;
+console.log( 'john  is '+age+' years  old '+'and  it is '+fullAge);
+ 
+var job;
+console.log(job);
+
+job = 'teacher';
+
+console.log('he  is a '+job);
+//var naming  rules
+/*fhff
+hhhhh*/
+
+var _3year = 3;
+console.log(_3year);
+
+var  job,  isMarried;
+
+job = "teacher";
+isMarried = false;
+
+/************************** */
+//typing coerection//
+
+console.log(fristName+ ' is  a '+job+'. he is '+age+' years  old.'+ 'but is he married? '+isMarried);
+
+
+/*******************************/
+
+//variable  mutation== changing  value  of a  variable
+
+age='tweenty eight';
+
+console.log(age);
+
+
+alert(fristName+ ' is  a '+job+'. he is '+age+' years  old.'+ 'but is he married? '+isMarried);
+
+
+var lastName = prompt('what  is  his  lastname?');
+
+console.log(lastName);
+
+
+console.log(fristName+" "+lastName);
+
+
+
+
